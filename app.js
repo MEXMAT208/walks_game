85354ad29a9062b677a895ad71df0043341adce2
nzhukovskiy <nzhukovskiy@okko.tv>
2026-08-25 19:42:45 +0300

    fix confetti

--- app.js
+++ app.js
@@ -1069,7 +1069,3 @@ document.addEventListener('DOMContentLoaded', async () => {
 });
 
 window.addEventListener('contextmenu', event => event.preventDefault());
-
-document.addEventListener('click', function() {
-    confetti({ particleCount: 50 });
-});
